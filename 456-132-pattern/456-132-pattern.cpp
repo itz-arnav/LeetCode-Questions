ba01d39efda653ea7c171c73deffbed2af3b1672
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-05-07 11:43:37 +0530

    Time: 279 ms (6.37%), Space: 56.6 MB (5.82%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -1,15 +1,52 @@
 class Solution {
 public:
-     bool find132pattern(vector<int>& nums) {
-        int s3 = INT_MIN;
-        stack<int> st;
-        for( int i = nums.size()-1; i >= 0; i -- ){
-            if( nums[i] < s3 ) return true;
-            else while( !st.empty() && nums[i] > st.top() ){ 
-              s3 = st.top(); st.pop(); 
-            }
-            st.push(nums[i]);
+     bool find132pattern(vector<int>& arr) {
+        
+        int n = arr.size();
+        vector<int> left(n), right(n); 
+        
+        left[0] = arr[0];
+        for(int i = 1; i<n; i++){
+          if(left[i-1] < arr[i] or arr[i-1] < arr[i]){
+              left[i] = min(left[i-1], arr[i-1]);
+          } 
+          else{
+              left[i] = left[i-1];
+          }  
         }
+         
+        set<int> st;
+        st.insert(arr[n-1]);
+        for(int j = n-2; j>=0; j--){
+            auto it = st.lower_bound(arr[j]);
+            if(it == st.end()){
+                it--;
+                if(*it >= arr[j])
+                    right[j] = INT_MAX;
+                else
+                    right[j] = *it;
+            }
+            else if(it == st.begin() and *it >= arr[j]){
+                right[j] = INT_MAX; 
+            }
+            else if(it == st.begin()){
+                right[j] = *it;
+            }
+            else{
+                it--;
+                right[j] = *it;
+            }
+            st.insert(arr[j]);
+        }
+        
+         // for(int i = 0; i<n; i++){
+         //     cout<<left[i]<<" "<<right[i]<<endl;
+         // }
+        for(int i = 1; i<n-1; i++){
+            if(left[i] < arr[i] and right[i] < arr[i] and right[i] > left[i])
+                return true;
+        }
+         
         return false;
     }
 };
