6cb72d9e7cd539b96a3f0c09075214f1de0d1722
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-05-09 11:35:21 +0530

    Time: 258 ms (9.41%), Space: 56.7 MB (5.21%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -1,50 +1,28 @@
 class Solution {
 public:
-     bool find132pattern(vector<int>& arr) {
+     bool find132pattern(vector<int>& nums) {
         
-        int n = arr.size();
-        vector<int> left(n), right(n); 
+        int n = nums.size();
+    vector<int> left = nums, right(n, INT_MAX);
+    
+    for(int i = 1; i < n; i++)
+        left[i] = min(left[i - 1], nums[i]);
+    
+    map<int, bool> mp;
+    mp[nums[n - 1]] = true;
+    for(int i = n - 2; i >= 0; i--)
+    {
+        auto it = mp.lower_bound(nums[i]);
+        if(it != mp.begin())
+            right[i] = (--it)->first;
         
-        left[0] = arr[0];
-        for(int i = 1; i<n; i++){
-          if(left[i-1] < arr[i] or arr[i-1] < arr[i]){
-              left[i] = min(left[i-1], arr[i-1]);
-          } 
-          else{
-              left[i] = left[i-1];
-          }  
-        }
-         
-        set<int> st;
-        st.insert(arr[n-1]);
-        for(int j = n-2; j>=0; j--){
-            auto it = st.lower_bound(arr[j]);
-            if(it == st.end()){
-                it--;
-                if(*it >= arr[j])
-                    right[j] = INT_MAX;
-                else
-                    right[j] = *it;
-            }
-            else if(it == st.begin() and *it >= arr[j]){
-                right[j] = INT_MAX; 
-            }
-            else if(it == st.begin()){
-                right[j] = *it;
-            }
-            else{
-                it--;
-                right[j] = *it;
-            }
-            st.insert(arr[j]);
-        }
-        
-        
-        for(int i = 1; i<n-1; i++){
-            if(left[i] < arr[i] and right[i] < arr[i] and right[i] > left[i])
-                return true;
-        }
-         
-        return false;
+        mp[nums[i]] = true;
+    }
+    
+    for(int i = 1; i < n - 1; i++)
+        if(right[i] > left[i - 1] && nums[i] > right[i])
+            return true;
+    
+    return false;
     }
 };
