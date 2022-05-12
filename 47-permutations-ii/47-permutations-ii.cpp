e247e669d58c52f51a6840e5f7375b3d309c191f
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-05-12 08:10:59 +0530

    Time: 3 ms (97.10%), Space: 8.5 MB (89.32%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    void solve(vector<int> curr, int i, int j, vector<vector<int> > &arr) {
-        if (i == j-1) {
-            arr.push_back(curr);
-            return;
-        }
-        for (int k = i; k < j; k++) {
-            if (i != k && curr[i] == curr[k]) continue;
-            swap(curr[i], curr[k]);
-            solve(curr, i+1, j, arr);
-        }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans; 
+        ans.push_back(nums);
+        while(next_permu(nums))
+            ans.push_back(nums);
+        return ans;
     }
-    vector<vector<int> > permuteUnique(vector<int> &num) {
-        sort(num.begin(), num.end());
-        vector<vector<int> >arr;
-        solve(num, 0, num.size(), arr);
-        return arr;
+    
+    bool next_permu(vector<int> &nums){
+        int i;
+        int n = nums.size();
+        for(i = n - 2; i >= 0 and nums[i] >= nums[i+1]; i--);
+        if(i < 0) return false;
+        int j;
+        for(j = n - 1; j >= 0 and nums[j] <= nums[i]; j--);
+        
+        swap(nums[j],nums[i]);        
+        reverse(nums.begin() + i + 1, nums.end());
+        return true;
     }
 };
