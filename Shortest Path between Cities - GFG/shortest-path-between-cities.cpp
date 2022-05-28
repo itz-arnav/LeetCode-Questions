70554f4ff14c6163dc702f4db87aaff788b48ae1
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-05-28 12:17:09 +0530

    Added solution - LeetHub

---
+++ Shortest Path between Cities - GFG/shortest-path-between-cities.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int shortestPath( int x, int y){ 
+        // code here 
+        int ans=0;
+        while(true)
+        {
+            if(x==y)
+                break;
+            if(x>y)
+                x=x/2;
+            else if(y>x)
+                y=y/2;
+            ans++;    
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y;
+        cin>>x>>y;
+        Solution ob;
+        cout<< ob.shortestPath(x,y) << endl;
+    }
+    return 0; 
+}   // } Driver Code Ends
