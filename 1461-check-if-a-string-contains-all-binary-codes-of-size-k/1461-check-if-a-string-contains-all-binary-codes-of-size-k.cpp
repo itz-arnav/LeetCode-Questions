f881d606176a3ff9096e78fbeebbfa53c17cd02e
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-06-01 01:06:03 +0530

    Time: 311 ms (71.43%), Space: 39.9 MB (77.02%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
+    int power(int a, int n){
+        if(n == 0)
+            return 1;
+        if(n == 1)
+            return a;
+        
+        int res = 1;
+        while(n){
+            if(n&1){
+                res *= a;
+                n--;
+            }
+            else{
+                a*=a;
+                n>>=1;
+            }
+        }
+        return res;
+    }
     bool hasAllCodes(string s, int k) {
         int n = s.size();
         if (k > n) return false;
-        
         unordered_set<int> st;
-        
         for (int i = 0; i + k <= n; i++){
-            
             int temp = 0;
             for(int j = 0; j<k; j++){
                 int curr = s[i+j] - '0';
                 temp |= (curr<<j);
             }
-            
-            st.insert(temp);
-            
+            st.insert(temp);  
         }
-        
-        return st.size() == pow(2, k);
+        int total_subsets = power(2,k);
+        return (st.size() == total_subsets);
+            
     }
 };
