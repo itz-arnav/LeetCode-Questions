8d9c286f50eea592e48c76ffd31ecff2b4c5e13e
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-06-03 23:23:08 +0530

    Time: 1110 ms (5.02%), Space: 165.7 MB (5.15%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -1,27 +1,40 @@
 class NumMatrix {
-private:
-    int row, col;
-    vector<vector<int>> sums;
 public:
-    NumMatrix(vector<vector<int>> &matrix) {
-        row = matrix.size();
-        col = row>0 ? matrix[0].size() : 0;
-        sums = vector<vector<int>>(row+1, vector<int>(col+1, 0));
-        for(int i=1; i<=row; i++) {
-            for(int j=1; j<=col; j++) {
-                sums[i][j] = matrix[i-1][j-1] + 
-                             sums[i-1][j] + sums[i][j-1] - sums[i-1][j-1] ;
-            }
-        }
+   vector<vector<long>> l;
+    NumMatrix(vector<vector<int>>& matrix) {
+        long sum=0;
+        
+       for(int i=0;i<matrix.size();i++)
+       {
+           sum=0;
+           vector<long> v;
+           for(int j=0;j<matrix[i].size();j++)
+           {
+               sum=sum+matrix[i][j];
+               v.push_back(sum);
+           }
+           
+           l.push_back(v);
+       }
+        
+//         for(int i = 0; i<l.size(); ++i){
+//             for(int j = 0; j<l[i].size(); ++j){
+//                 cout<<l[i][j]<<" ";
+//             }
+//             cout<<endl;
+//         }
+        
     }
-
+    
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return sums[row2+1][col2+1] - sums[row2+1][col1] - sums[row1][col2+1] + sums[row1][col1];
+        int sum=0;
+        for(int i=row1;i<=row2;i++)
+        {
+            if(col1!=0)
+                sum=sum + l[i][col2] - l[i][col1-1];
+            else
+                sum=sum+l[i][col2];
+        }
+        return sum;
     }
 };
-
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix* obj = new NumMatrix(matrix);
- * int param_1 = obj->sumRegion(row1,col1,row2,col2);
- */
