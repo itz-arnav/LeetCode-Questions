3912e8a035cb23ffabd4421c5f0b815899cfba78
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-06-05 11:55:50 +0530

    Time: 583 ms (42.86%), Space: 110.7 MB (100.00%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
@@ -1,15 +1,13 @@
+int m[1000001];
 class Solution {
 public:
-    unordered_map<int,int> mp;
-
-    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
-        for(int i=0 ; i < nums.size() ; i++)
-            mp[nums[i]] = i; //Save position of each value
-        for(vector<int> a : operations) mp[a[1]] = mp[a[0]] , mp[a[0]] = -1; 
-		// When a-> b we give position of a to b, mark that a is not in the array anymore
-        for(auto i : mp)
-            if(i.second >= 0) // If value is in array
-                nums[i.second] = i.first;
-        return nums;
+vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
+    for (int i = 0; i < nums.size(); ++i)
+        m[nums[i]] = i;
+    for (auto &op : operations) {
+        nums[m[op[0]]] = op[1];
+        m[op[1]] = m[op[0]];
     }
+    return nums;
+}
 };
