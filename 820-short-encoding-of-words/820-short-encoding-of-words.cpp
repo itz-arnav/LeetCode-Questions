490e0cc45bfbf8470cd5431d268ebc1c0a8b4998
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-06-20 20:56:29 +0530

    Time: 2122 ms (5.45%), Space: 13.7 MB (98.51%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,12 +1,36 @@
 class Solution {
 public:
-      int minimumLengthEncoding(vector<string>& words) {
-        unordered_set<string> s(words.begin(), words.end());
-        for (string w : s)
-            for (int i = 1; i < w.size(); ++i)
-                s.erase(w.substr(i));
-        int res = 0;
-        for (string w : s) res += w.size() + 1;
-        return res;
+    int minimumLengthEncoding(vector<string>& words) {
+        int ans=0,n=words.size();
+        unordered_map<string, bool>mp;
+        for(int i=0;i<n;i++){
+            bool f=false;
+            bool twice = false;
+            int s1=words[i].size();
+            for(int j=0;j<n;j++){
+                if(i==j)
+                    continue;
+                int s2=words[j].size();
+                int a=s1-1,b=s2-1;
+             //   int m=max(s1,s2);
+                
+                while(a>=0 && b>=0){
+                    if(words[i][a]!=words[j][b]) break;
+                    a--; b--;
+                }
+                if(a == -1 and b == -1){
+                    twice = true;
+                    
+                }
+                if(a<0) f=true;
+            }
+            if(!f) ans+=s1+1;
+            else if(twice and mp[words[i]]==false){
+                ans += s1+1;
+                mp[words[i]] = true;
+            }
+        }
+        //if(!ans) ans+=words[0].size()+1;
+        return ans;
     }
 };
