80bf0b73815fd45920717ddf565399137892813d
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-07-03 23:18:36 +0530

    Added solution - LeetHub

---
+++ Print all nodes that don't have sibling - GFG/print-all-nodes-that-dont-have-sibling.cpp
@@ -0,0 +1,140 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+vector<int> noSibling(Node *root);
+
+int main()
+{
+    int t;
+    scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+        
+        vector<int> res = noSibling(root);
+        for (int i = 0; i < res.size(); ++i)
+        {
+            cout<<res[i]<<" ";
+        }
+        cout<<endl;
+  }
+  return 0;
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+/* Tree node structure  used in the program
+ struct Node
+ {
+     int data;
+     Node* left, *right;
+}; */
+
+void solve(Node* baap, Node* node, vector<int> &ans){
+    if(node == 0)return;
+    if(baap == 0)return;
+    if(baap->left == 0 and baap->right != 0){
+        ans.push_back(node->data);
+    }
+    else if(baap->right == 0 and baap->left != 0){
+        ans.push_back(node->data);
+    }
+   
+        solve(node, node->right, ans);
+        solve(node, node->left, ans);
+    
+}
+
+vector<int> noSibling(Node* node)
+{
+    // code here
+    vector<int> ans;
+    solve(node, node->left, ans);
+    solve(node, node->right, ans);
+    sort(ans.begin(), ans.end());
+    if(ans.size() == 0)
+    ans.push_back(-1);
+    return ans;
+}
