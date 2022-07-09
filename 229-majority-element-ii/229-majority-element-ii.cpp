0fb6602b3eacf0ab628d4aaf430c8b0f931be3e1
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-07-09 16:13:28 +0530

    Time: 23 ms (41.00%), Space: 15.8 MB (51.49%) - LeetHub

---
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& a) {
+        int y(-1), z(-1), cy(0), cz(0);
+    
+	  for (const auto & x: a) 
+	  {
+		  if (x == y) cy++;
+		  else if (x == z) cz++;
+		  else if (! cy) y = x, cy = 1;
+		  else if (! cz) z = x, cz = 1;
+		  else cy--, cz--;
+     }
+      
+     cy = cz = 0;
+     for (const auto & x: a)
+		 if (x == y) cy++;
+			 else if (x == z) cz++;
+  
+	  vector<int> r;
+	  if (cy > size(a)/3) r.push_back(y);
+	  if (cz > size(a)/3) r.push_back(z);
+	  return r;
+    }
+};
