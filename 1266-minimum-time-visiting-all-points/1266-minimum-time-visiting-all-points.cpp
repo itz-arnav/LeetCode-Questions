80ecec3e3c7b29346b26d80dbc06e4d00c57ec2f
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-07-31 19:14:25 +0530

    Time: 18 ms (28.25%), Space: 10.2 MB (21.75%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -1,41 +1,10 @@
 class Solution {
 public:
-    int minTimeToVisitAllPoints(vector<vector<int>>& arr) {
-        int x = arr[0][0];
-        int y = arr[0][1];
-        int n = arr.size();
-        int ct = 0;
-        for(int i = 1; i<n; ++i){
-            while(x != arr[i][0] or y != arr[i][1]){
-                if(x == arr[i][0]){
-                    if(y > arr[i][1])
-                        y--;
-                    else
-                        y++;
-                }
-                else if(y == arr[i][1]){
-                    if(x < arr[i][0]){
-                        x++;
-                    }
-                    else
-                        x--;
-                }
-                else{
-                    if(x < arr[i][0]){
-                        x++;
-                    }
-                    else{
-                        x--;
-                    }
-                    if(y < arr[i][1]){
-                        y++;
-                    }
-                    else
-                        y--;
-                }
-                ct++;
-            }
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int ans = 0;
+        for(int i = 1; i < points.size(); i++) {
+            ans += max(abs(points[i][1] - points[i - 1][1]), abs(points[i][0] - points[i - 1][0]));
         }
-        return ct;
+        return ans;
     }
 };
