4271d451c62a70151d12b18844e6967e371176df
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-07-31 19:43:01 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (50.53%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        return stoi(to_string(num).replace(to_string(num).find('6')!=string::npos?to_string(num).find('6'):0,1,"9"));
+        int temp = num, leftmost_six=-1;
+    for(int i=1; temp > 0; i *= 10, temp /= 10 )
+         if( temp%10 == 6 ) leftmost_six = i;
+    return ( leftmost_six == -1 ? num : num + 3 * leftmost_six );
     }
 };
