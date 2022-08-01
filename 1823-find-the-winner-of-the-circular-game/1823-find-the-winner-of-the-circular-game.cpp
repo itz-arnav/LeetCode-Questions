14822c5c7369477527ac4121cbeac80f6a91c0e1
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-08-01 11:40:37 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.44%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int helper(int n,int k){
-        int ans = 0;
-        for(int i=1; i<=n; i++){
-            ans = (ans + k) % i;
-        }
-        return ans;
+        if(n==1)
+            return 0;
+        return (helper(n-1,k) + k) % n;
     }
     int findTheWinner(int n, int k) {
-        return helper(n,k)+1;
+        return helper(n,k)+1;   //+1 is for converting 0-based indexing to 1-based indexing
     }
 };
