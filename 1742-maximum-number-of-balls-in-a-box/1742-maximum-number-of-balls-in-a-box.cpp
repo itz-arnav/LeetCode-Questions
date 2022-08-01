4a8b5ab73d5a3430ade42322f46a9601f7c6550a
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-08-01 13:45:36 +0530

    Time: 7 ms (95.72%), Space: 5.9 MB (90.25%) - LeetHub

--- 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.cpp
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int countBalls(int l, int h) {
-        unordered_map<int,int> mp;
-        int best = 0;
-        vector<int> dp(h + 5, 0);
-        for(int i = 1; i<=h; ++i){
-            dp[i] = i%10 + dp[i/10];
+        int cnt[46] = {};
+    for (auto i = l; i <= h; ++i) {
+        int sum = 0, n = i;
+        while(n) {
+            sum += n % 10;
+            n /= 10; 
         }
-        for(int i = l; i<=h; ++i){
-            mp[dp[i]]++;
-            best = max(best, mp[dp[i]]);
-        }
-        return best;
+        ++cnt[sum];
+    }
+    return *max_element(begin(cnt), end(cnt));
         
     }
 };
