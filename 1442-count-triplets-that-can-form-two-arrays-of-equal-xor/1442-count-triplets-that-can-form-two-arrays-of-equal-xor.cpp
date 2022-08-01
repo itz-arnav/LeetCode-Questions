ed107f5b3525ada99851736b230811ec15ee2355
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-08-01 11:20:29 +0530

    Time: 3 ms (84.82%), Space: 7.7 MB (14.01%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -5,8 +5,10 @@ public:
         unordered_map<int, int> count = {{0, 1}}, total;
         for (int i = 0; i < n; ++i) {
             prefix ^= A[i];
-            res += count[prefix]++ * i - total[prefix];
-            total[prefix] += i + 1;
+            res += count[prefix]*i - total[prefix];
+            // cout<<"count added : "<<count[prefix]*i<<" total : "<<total[prefix]<<endl;
+            count[prefix]++;
+            total[prefix] += (i+1);
         }
         return res;
     }
