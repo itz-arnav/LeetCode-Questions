ac5595e8e60b5bdd7f184b6b4a64cace3e37f65f
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-08-02 20:20:46 +0530

    Time: 205 ms (5.03%), Space: 20.6 MB (5.69%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,24 +1,17 @@
-class Solution { // 20 ms, faster than 98.92%
+class Solution { // 124 ms, faster than 5.10%
 public:
-    int m, n;
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        m = matrix.size(), n = matrix[0].size(); // For general, the matrix need not be a square
-        int left = matrix[0][0], right = matrix[m-1][n-1], ans = -1;
-        while (left <= right) {
-            int mid = (left + right) >> 1;
-            if (countLessOrEqual(matrix, mid) >= k) {
-                ans = mid;
-                right = mid - 1; // try to looking for a smaller value in the left side
-            } else left = mid + 1; // try to looking for a bigger value in the right side
+    int kthSmallest(vector<vector<int>> &matrix, int k) {
+        int m = matrix.size(), n = matrix[0].size(), ans; // For general, the matrix need not be a square
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> minHeap;
+        for (int r = 0; r < min(m, k); ++r)
+            minHeap.push({matrix[r][0], r, 0});
+
+        for (int i = 1; i <= k; ++i) {
+            auto top = minHeap.top(); minHeap.pop();
+            int r = top[1], c = top[2];
+            ans = top[0];
+            if (c + 1 < n) minHeap.push({matrix[r][c + 1], r, c + 1});
         }
         return ans;
-    }
-    int countLessOrEqual(vector<vector<int>>& matrix, int x) {
-        int cnt = 0, c = n - 1; // start with the rightmost column
-        for (int r = 0; r < m; ++r) {
-            while (c >= 0 && matrix[r][c] > x) --c;  // decrease column until matrix[r][c] <= x
-            cnt += (c + 1);
-        }
-        return cnt;
     }
 };
