626aff099d8f5124ae144c2487eef1efde598f76
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-08-12 23:24:28 +0530

    Time: 266 ms (14.15%), Space: 11.8 MB (38.77%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,16 +1,44 @@
 class Solution {
 public:
-    int numFactoredBinaryTrees(vector<int>& A) {
-        long res = 0, mod = pow(10, 9) + 7;
-        sort(A.begin(), A.end());
-        unordered_map<int, long> dp;
-        for (int i = 0; i < A.size(); ++i) {
-            dp[A[i]] = 1;
-            for (int j = 0; j < i; ++j)
-                if (A[i] % A[j] == 0)
-                    dp[A[i]] = (dp[A[i]] + dp[A[j]] * dp[A[i] / A[j]]) % mod;
-            res = (res + dp[A[i]]) % mod;
+     int mod=1000000007;
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        map<int,vector<pair<int,int>>> mp;
+        map<int,long> dp;
+        sort(arr.begin(),arr.end());
+        for(int i = 0; i<arr.size(); ++i){
+            for(int j = 0; j<arr.size(); j++){
+                int other = arr[i]/arr[j];
+                if(arr[i]%arr[j] == 0){
+                    if(binary_search(arr.begin(),arr.end(),other))
+                    {
+                       mp[arr[i]].push_back({arr[j],other});
+                    }
+                }
+            }
         }
-        return res;
+        long long ans=0;
+        for(int i = 0; i<arr.size(); ++i){
+        long s = 1;
+        for(auto x : mp[arr[i]]){
+                s= (s + 1LL*dp[x.first]*dp[x.second])%mod;
+            
+        }
+        dp[arr[i]]=s;
+        ans=(ans+dp[arr[i]])%mod;
+        }
+        // long res = 0;
+        // sort(arr.begin(), arr.end());
+        // for (int i = 0; i < arr.size(); ++i) {
+        //     dp[arr[i]] = 1;
+        //     for (int j = 0; j < i; ++j)
+        //         if (arr[i] % arr[j] == 0)
+        //             dp[arr[i]] = (dp[arr[i]] + dp[arr[j]] * dp[arr[i] / arr[j]]) % mod;
+        //     res = (res + dp[arr[i]]) % mod;
+        // }
+        for(int i = 0; i<arr.size(); ++i){
+            cout<<arr[i]<<" : "<<dp[arr[i]]<<endl;
+        }
+        cout<<endl;
+        return ans;
     }
 };
