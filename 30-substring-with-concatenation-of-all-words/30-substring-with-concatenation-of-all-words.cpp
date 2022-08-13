b6021098bc7ca367266bfdcf066646b7c9c611b4
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-08-13 09:31:14 +0530

    Time: 14 ms (99.81%), Space: 13.4 MB (97.37%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    double hash(double f, double code[], string &word) {
+        double result = 0.;
+        for (auto &c : word) result = result * f + code[c];
+        return result;
+    }
+    vector<int> findSubstring(string S, vector<string> &L) {
+        uniform_real_distribution<double> unif(0., 1.);
+        default_random_engine seed;
+        double code[128];
+        for (auto &d : code) d = unif(seed);
+        double f = unif(seed) / 5. + 0.8;
+        double value = 0;
+
+        // The complexity of the following for loop is O(L.size( ) * nW).
+        for (auto &str : L) value += log(hash(f, code, str));
+
+        int unit = 1e9;
+        int key = (value-floor(value))*unit;
+        int nS = S.size(), nL = L.size(), nW = L[0].size();
+        double fn = pow(f, nW-1.);
+        vector<int> result;
+        if (nS < nW) return result;
+        vector<double> values(nS-nW+1);
+        string word(S.begin(), S.begin()+nW);
+        values[0] = hash(f, code, word);
+
+        // Use a moving window to hash every word with length nW in S to a float number, 
+        // which is stored in vector values[]
+        // The complexity of this step is O(nS).
+        for (int i=1; i<=nS-nW; ++i) values[i] = (values[i-1] - code[S[i-1]]*fn)*f + code[S[i+nW-1]];
+
+        // This for loop will run nW times, each iteration has a complexity O(nS/nW)
+        // So the overall complexity is O(nW * (nS / nW)) = O(nS)
+        for (int i=0; i<nW; ++i) {
+            int j0=i, j1=i, k=0;
+            double sum = 0.;
+
+            // Use a moving window to hash every L.size() continuous words with length nW in S.
+            // This while loop will terminate within nS/nW iterations since the increasement of j1 is nW,
+            // So the complexity of this while loop is O(nS / nW).
+            while(j1<=nS-nW) {
+                sum += log(values[j1]);
+                ++k;
+                j1 += nW;
+                if (k==nL) {
+                    int key1 = (sum-floor(sum)) * unit;
+                    if (key1==key) result.push_back(j0);
+                    sum -= log(values[j0]);
+                    --k;
+                    j0 += nW;
+                }
+            }
+        }
+        return result;
+    }
+};
