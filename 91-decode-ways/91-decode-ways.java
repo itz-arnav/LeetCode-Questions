c893be49443236ae0845791ec69d00e07ebaf3d8
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-10-01 18:53:42 +0530

    Time: 1 ms (98.72%), Space: 42 MB (73.09%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int numDecodings(String s) {
+        
+         if (s.isEmpty() || s.charAt(0) - '0' == 0)
+    {
+        return 0;
+    }
+    
+    int[] waysToDecode = new int[s.length() + 1];
+    waysToDecode[0] = 1;
+    waysToDecode[1] = 1;
+    for (int i = 1; i < s.length(); i++)
+    {
+        int curr = s.charAt(i) - '0';
+        int prev = s.charAt(i - 1) - '0';
+        
+        // can't make progress, return 0
+        if (prev == 0 && curr == 0 || (curr == 0 && (prev * 10 + curr > 26)))
+        {
+            return 0;
+        }
+        // can't use the previous value, so can only get to this state from the previous
+        else if (prev == 0 || (prev * 10 + curr) > 26)
+        {
+            waysToDecode[i + 1] = waysToDecode[i];
+        }
+        // can't use current state, can only get to this state from i - 1 state
+        else if (curr == 0)
+        {
+            waysToDecode[i + 1] = waysToDecode[i - 1];
+        }
+        // can get to this state from the previous two states
+        else
+        {
+            waysToDecode[i + 1] = waysToDecode[i] + waysToDecode[i - 1];
+        }
+    }
+    
+    return waysToDecode[waysToDecode.length - 1];
+        
+    }
+}
