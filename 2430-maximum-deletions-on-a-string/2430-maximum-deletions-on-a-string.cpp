a6b8a4a60b557007682f7be55cddee7f84af4f09
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-10-02 12:35:02 +0530

    Time: 1150 ms (12.50%), Space: 227.4 MB (50.00%) - LeetHub

--- 2430-maximum-deletions-on-a-string/2430-maximum-deletions-on-a-string.cpp
+++ 2430-maximum-deletions-on-a-string/2430-maximum-deletions-on-a-string.cpp
@@ -1,46 +1,17 @@
 class Solution {
 public:
-    string s;
-    vector<int> memo;
-    int n;
-    
-    // taken from cp-algorithms
-    vector<int> z_function(string& s) {
-        int n = (int) s.length();
-        vector<int> z(n);
-        for (int i = 1, l = 0, r = 0; i < n; ++i) {
-            if (i <= r)
-                z[i] = min (r - i + 1, z[i - l]);
-            while (i + z[i] < n && s[z[i]] == s[i + z[i]])
-                ++z[i];
-            if (i + z[i] - 1 > r)
-                l = i, r = i + z[i] - 1;
-        }
-        return z;
-    }
-    
-    int solve(int i){
-        if(i == s.size()) return 0;
-        if(memo[i] != -1) return memo[i];
-        
-        int result = 1;
-        string sub = s.substr(i);
-        vector<int> z = z_function(sub);
-        int j = 1;
-        
-        while(j < z.size()){
-            if(z[j] >= j) result = max(result, 1 + solve(i+j));
-            j++;
-        }
-        
-        return memo[i] = result;
-    }
-    
     int deleteString(string s) {
-        this->memo = vector<int>(s.size(), -1);
-        this->n = s.size();
-        this->s = s;
-        
-        return solve(0);
+        int n = s.size();
+        vector<vector<int>> lcs(n + 1, vector<int>(n + 1, 0));
+        vector<int> dp(n, 1);
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (s[i] == s[j])
+                    lcs[i][j] = lcs[i + 1][j + 1] + 1;
+                if (lcs[i][j] >= j - i)
+                    dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        return dp[0];
     }
 };
