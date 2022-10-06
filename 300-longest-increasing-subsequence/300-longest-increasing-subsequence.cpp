8a17e497e490e4b4d5ceda63d9f04b0ba974123d
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-10-06 19:07:24 +0530

    Time: 773 ms (17.85%), Space: 10.4 MB (87.89%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
-    // int f(int i,vector<int> &v,vector<vector<int>> &dp,int prev,int n){
-    //     if(i>=n) return 0;
-    //     if(dp[i][prev]!=-1) return dp[i][prev];
-    //     int npick=f(i+1,v,dp,prev,n);
-    //     int pick=0;
-    //     if(prev==0) 
-    //         pick=1 + f(i+1,v,dp,i+1,n);
-    //     else if(v[i]>v[prev-1])
-    //         pick=1 + f(i+1,v,dp,i+1,n);
-    //     return dp[i][prev]=max(pick,npick);
-    // }
     int lengthOfLIS(vector<int>& arr) {
         int n = arr.size();
-        vector<vector<int>> t(n+10, vector<int>(n+10,0));
-        for(int j =0; j< n+1; j++)
-        {
-            t[0][j] = 0;
-            
-        }
-        
-        for(int i =1; i< n+1; i++)
-        {
-            for(int j =1; j<n+1; j++)
-            {
-                if(j == n || arr[i-1] < arr[j])
-                {
-                    t[i][j] = max(t[i-1][j], t[i-1][i-1]+1);
+        vector<int> dp(n+1, 0);
+        int best = 1;
+        for(int i = 0; i<n; ++i){
+            dp[i] = 1;
+            for(int j = 0; j<i; ++j){
+                if(arr[i] > arr[j]){
+                    dp[i] = max(dp[i], 1 + dp[j]);
+                    best = max(best, dp[i]);
                 }
-                else
-                    t[i][j] = t[i-1][j];
-                
             }
         }
-        
-        return t[n][n];
+        return best;
     }
-    
 };
