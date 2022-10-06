0536c27931a35649641f7357494862580ce2fe03
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-10-06 12:49:28 +0530

    Time: 752 ms (21.38%), Space: 130.8 MB (19.24%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -1,19 +1,23 @@
 class TimeMap {
 public:
-    unordered_map<string, vector<pair<int, string>>> m;
-void set(string key, string value, int timestamp) {
-  m[key].push_back({ timestamp, value });
-}
-string get(string key, int timestamp) {
-  auto it = upper_bound(begin(m[key]), end(m[key]), pair<int, string>(timestamp, ""), [](
-    const pair<int, string>& a, const pair<int, string>& b) { return a.first < b.first; });
-  return it == m[key].begin() ? "" : prev(it)->second;
-}
+    unordered_map<string, map<int, string>> maps;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        maps[key][timestamp] = value;
+    }
+    
+    string get(string key, int timestamp) {
+        if (!maps.count(key)) {
+            return "";
+        }
+        auto it = maps[key].upper_bound(timestamp);
+        if (it != maps[key].begin()) {
+            return (--it)->second;
+        } else {
+            return "";
+        }
+    }
 };
-
-/**
- * Your TimeMap object will be instantiated and called as such:
- * TimeMap* obj = new TimeMap();
- * obj->set(key,value,timestamp);
- * string param_2 = obj->get(key,timestamp);
- */
