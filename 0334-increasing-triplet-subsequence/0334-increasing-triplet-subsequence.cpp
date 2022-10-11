7e9ed453633f68d9c209f00a5aa76f8567b5ee9c
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-10-11 08:22:12 +0530

    Time: 126 ms (52.23%), Space: 61.5 MB (87.68%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        vector<int> ans;
-        for(int i = 0; i<nums.size(); ++i){
-            if(ans.size() == 0){
-                ans.push_back(nums[i]);
-            }
-            else{
-                auto it = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
-                if(it >= ans.size()){
-                    ans.push_back(nums[i]);
-                }
-                else{
-                    ans[it] = nums[i];
-                }
-            }
+    int c1 = INT_MAX, c2 = INT_MAX;
+    for (int x : nums) {
+        if (x <= c1) {
+            c1 = x;           // c1 is min seen so far (it's a candidate for 1st element)
+        } else if (x <= c2) { // here when x > c1, i.e. x might be either c2 or c3
+            c2 = x;           // x is better than the current c2, store it
+        } else {              // here when we have/had c1 < c2 already and x > c2
+            return true;      // the increasing subsequence of 3 elements exists
         }
-        return ans.size() >= 3;
     }
+    return false;
+}
 };
