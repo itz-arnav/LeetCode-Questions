750a9350c63f1380a898432ceed3225c8a57a035
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-10-12 11:22:58 +0530

    Time: 103 ms (8.79%), Space: 9.9 MB (33.94%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
-   
-    bool stoneGame(vector<int>& arr) {
-        return true;
+    pair<int, int> memo[501][501] = {};
+    bool stoneGame(vector<int>& piles) {
+        auto [aliceScore, leeScore] = dp(piles, 0, piles.size() - 1);
+        return aliceScore > leeScore;
+    }
+    pair<int, int> dp(vector<int>& piles, int left, int right) {
+        if (left > right) return make_pair(0, 0);
+        if (memo[left][right].first != 0) return memo[left][right];
+
+        pair<int, int> leftPick = dp(piles, left + 1, right);
+        pair<int, int> rightPick = dp(piles, left, right - 1);
+        if (piles[left] + leftPick.second > piles[right] + rightPick.second) { // If the left choice has higher score than the right choice
+            return memo[left][right] = make_pair(piles[left] + leftPick.second, leftPick.first); // then pick left
+        }
+        return memo[left][right] = make_pair(piles[right] + rightPick.second, rightPick.first); // else pick right
     }
 };
