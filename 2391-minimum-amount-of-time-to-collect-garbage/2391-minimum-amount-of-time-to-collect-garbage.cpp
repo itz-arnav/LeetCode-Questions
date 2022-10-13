07374b35b96a04710718f594c47b90464784a791
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-10-13 10:15:29 +0530

    Time: 1519 ms (5.00%), Space: 212.9 MB (5.11%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& arr, vector<int>& travel) {
+        int time = 0, trav = 0;
+        
+        bool g = 0, p = 0, m = 0;
+        
+        for(auto x : travel)
+            trav += x;
+        
+        int n = arr.size();
+        for(int i = n-1; i>=0; --i){
+            if(i < n-1)
+            trav -= travel[i];
+            unordered_map<char,int> mp;
+            for(auto c : arr[i])
+                mp[c]++;
+            
+            if(mp['G'] > 0){
+                time += mp['G'];
+                if(g == 0){
+                    g = 1;
+                    time += trav;
+                }
+            }
+            if(mp['P'] > 0){
+                time += mp['P'];
+                if(p == 0){
+                    p = 1;
+                    time += trav;
+                }
+            }
+            if(mp['M'] > 0){
+                time += mp['M'];
+                if(m == 0){
+                    m = 1;
+                    time += trav;
+                }
+            }
+        }
+        return time;
+    }
+};
