d75bc7682f8365c85fffdf24454279cbab771739
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-10-13 10:17:43 +0530

    Time: 461 ms (29.43%), Space: 102.2 MB (75.42%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,43 +1,17 @@
 class Solution {
 public:
-    int garbageCollection(vector<string>& arr, vector<int>& travel) {
-        int time = 0, trav = 0;
-        
-        bool g = 0, p = 0, m = 0;
-        
-        for(auto x : travel)
-            trav += x;
-        
-        int n = arr.size();
-        for(int i = n-1; i>=0; --i){
-            if(i < n-1)
-            trav -= travel[i];
-            int mp[100] = {0};
-            for(auto c : arr[i])
-                mp[c]++;
-            
-            if(mp['G'] > 0){
-                time += mp['G'];
-                if(g == 0){
-                    g = 1;
-                    time += trav;
-                }
-            }
-            if(mp['P'] > 0){
-                time += mp['P'];
-                if(p == 0){
-                    p = 1;
-                    time += trav;
-                }
-            }
-            if(mp['M'] > 0){
-                time += mp['M'];
-                if(m == 0){
-                    m = 1;
-                    time += trav;
-                }
-            }
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        int last[128] = {}, res = 0;
+        for (int i = 0; i < garbage.size(); ++i) {
+            res += garbage[i].size();
+            for (char c : garbage[i])
+                last[c] = i;
         }
-        return time;
+        for (int j = 1; j < travel.size(); ++j)
+            travel[j] += travel[j - 1];
+        for (int c : "PGM")
+            if (last[c])
+                res += travel[last[c] - 1];
+        return res;
     }
 };
