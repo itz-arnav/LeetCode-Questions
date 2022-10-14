628fe56c12c57bce5b6baf919a79f45d5e90d22e
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-10-14 13:32:04 +0530

    Time: 186 ms (42.40%), Space: 27.9 MB (84.99%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
@@ -1,46 +1,48 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-      TreeNode* clone(TreeNode* root) {
-    TreeNode* new_root = new TreeNode(0);
-    new_root->left = (root->left) ? clone(root->left) : nullptr;
-    new_root->right = (root->right) ? clone(root->right) : nullptr; 
-    return new_root;
-  } 
-
-  vector<TreeNode*> allPossibleFBT(int N) {
-    std::vector<TreeNode*> ret;
-    if (1 == N) {
-      ret.emplace_back(new TreeNode(0));
-    } else if (N % 2) {
-      for (int i = 2; i <= N; i += 2) {
-        auto left = allPossibleFBT(i - 1);
-        auto right = allPossibleFBT(N - i);
-        for (int l_idx = 0; l_idx < left.size(); ++l_idx) {
-          for (int r_idx = 0; r_idx < right.size(); ++r_idx) {
-            ret.emplace_back(new TreeNode(0));
-
-            // If we're using the last right branch, then this will be the last time this left branch is used and can hence
-            // be shallow copied, otherwise the tree will have to be cloned
-            ret.back()->left = (r_idx == right.size() - 1) ? left[l_idx] : clone(left[l_idx]);
-
-            // If we're using the last left branch, then this will be the last time this right branch is used and can hence
-            // be shallow copied, otherwise the tree will have to be cloned
-            ret.back()->right = (l_idx == left.size() - 1) ? right[r_idx] : clone(right[r_idx]);
-          }
+    // global map
+    unordered_map<int, vector<TreeNode *>> memo;
+    
+    // recursion is very lengthy: memorization can help a little bit
+    vector<TreeNode *> allPossibleFBT(int n) {
+        // vector that stores ans
+        vector<TreeNode *> ans;
+        // base case
+        if (n < 1 || n % 2 == 0) {
+            // if n is less than 1 or a multiple of 2: cannot have a FULL binary tree
+            return ans;
         }
-      }
+        if (memo.find(n) != memo.end()) {
+            // memo map has something
+            return memo[n];
+        }
+        if (n == 1) {
+            // if n is eql to 1 simply return
+            ans.push_back(new TreeNode(0));
+            memo[1] = ans;
+            return ans;
+        }
+
+
+        // main part: including the recursive part
+        // get all the possible number of node combinations possible on the left and right sub tree
+        for (int i = 1; i < n; i += 2) {
+            // incremented i by 2 because if we increment it by 1 : it'll automatically get rejected by the base condition
+            // make 2 vectors that store the number of nodes in left and right sub tree specifically
+            vector<TreeNode *> left = allPossibleFBT(i);
+            vector<TreeNode *> right = allPossibleFBT(n - 1 - i);
+
+            // make trees from all possible combinations of left and right subtrees
+            for (int j = 0; j < left.size(); j++) {
+                for (int k = 0; k < right.size(); k++) {
+                    TreeNode *root = new TreeNode(0);
+                    root->left = left[j];
+                    root->right = right[k];
+                    ans.push_back(root);
+                }
+            }
+        }
+        memo[n] = ans;  // memorize the ans for this integer
+        return ans;
     }
-    return ret;
-  }
 };
