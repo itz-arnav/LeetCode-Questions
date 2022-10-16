eb5ac830fe696b18846b9de650fba3182fa12ca2
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-10-16 13:35:13 +0530

    Time: 36 ms (29.22%), Space: 11.8 MB (45.59%) - LeetHub

--- 0811-subdomain-visit-count/0811-subdomain-visit-count.cpp
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
-    vector<string> subdomainVisits(vector<string>& arr) {
-        unordered_map<string,int> mp;
-        for(int i = 0; i<arr.size(); ++i){
-        vector <string> tokens;
-        stringstream check1(arr[i]);
-        string intermediate;
-        while(getline(check1, intermediate, ' '))
-        {
-        tokens.push_back(intermediate);
+    vector<string> subdomainVisits(vector<string>& cpdomains) {
+        unordered_map<string, int> count;
+        for (auto cd : cpdomains) {
+            int i = cd.find(" ");
+            int n = stoi(cd.substr (0, i));
+            string s = cd.substr (i + 1);
+            for (int i = 0; i < s.size(); ++i)
+                if (s[i] == '.')
+                    count[s.substr(i + 1)] += n;
+            count[s] += n;
         }
-        int val = stoi(tokens[0]);
-        
-        string &temp = tokens[1];
-            int j = temp.size() - 1;
-            string t = "";
-            while(j >= 0){
-                if(temp[j] == '.'){
-                    mp[t] += val;
-                }
-                t.push_back(temp[j]);
-                j--;
-            }
-            reverse(temp.begin(), temp.end());
-            mp[temp] += (val);
-        }
-        
-        vector<string> ans;
-        for(auto &x : mp){
-            string temp = x.first;
-             reverse(temp.begin(), temp.end());
-            ans.push_back(to_string(x.second) + " " + temp);
-        }
-        return ans;
+        vector<string> res;
+        for (auto k : count)
+            res.push_back (to_string(k.second) + " " + k.first);
+        return res;
     }
 };
