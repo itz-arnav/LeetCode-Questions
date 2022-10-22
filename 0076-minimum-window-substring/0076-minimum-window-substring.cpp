02c2da689ef8d9cc62f40ad991733ea68c22e4dc
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-10-22 17:19:35 +0530

    Time: 31 ms (61.84%), Space: 7.8 MB (68.10%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int> map(128,0);
-        for(auto c: t) map[c]++;
-        int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
-        while(end<s.size()){
-            if(map[s[end++]]-->0) counter--; //in t
-            while(counter==0){ //valid
-                if(end-begin<d)  d=end-(head=begin);
-                if(map[s[begin++]]++==0) counter++;  //make it invalid
-            }  
-        }
-        return d==INT_MAX? "":s.substr(head, d);
+        unordered_map<char, int> m;
+	// Statistic for count of char in t
+	for (auto c : t) m[c]++;
+	// counter represents the number of chars of t to be found in s.
+	size_t start = 0, end = 0, counter = t.size(), minStart = 0, minLen = INT_MAX;
+	size_t size = s.size();
+	
+	// Move end to find a valid window.
+	while (end < size) {
+		// If char in s exists in t, decrease counter
+		if (m[s[end]] > 0)
+			counter--;
+		// Decrease m[s[end]]. If char does not exist in t, m[s[end]] will be negative.
+		m[s[end]]--;
+		end++;
+		// When we found a valid window, move start to find smaller window.
+		while (counter == 0) {
+			if (end - start < minLen) {
+				minStart = start;
+				minLen = end - start;
+			}
+			m[s[start]]++;
+			// When char exists in t, increase counter.
+			if (m[s[start]] > 0)
+				counter++;
+			start++;
+		}
+	}
+	if (minLen != INT_MAX)
+		return s.substr(minStart, minLen);
+	return "";
     }
 };
