db185ca125275685cf71984b8cb0996861d7d432
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-10-23 09:47:57 +0530

    Time: 2779 ms (7.39%), Space: 76.7 MB (10.02%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+    
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+    
+    def insert(self, node):
+        nxt = self.head.next
+        prev = self.head.next.prev
+        self.head.next = node
+        node.prev = self.head
+        node.next = nxt
+        node.next.prev = node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+        
+        if len(self.cache) > self.capacity:
+            lru = self.tail.prev
+            self.remove(lru)
+            del self.cache[lru.key]
