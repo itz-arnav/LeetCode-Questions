d16255ef664df967933b5432840c560f4b9633b8
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-10-27 08:53:54 +0530

    Time: 1545 ms (5.56%), Space: 15.6 MB (7.64%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
-        int n=img1.size();
-        vector<pair<int,int>>vec_a;
-        vector<pair<int,int>>vec_b;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(img1[i][j]==1){
-                    vec_a.push_back({i,j});
-                }
-                if(img2[i][j]==1){
-                    vec_b.push_back({i,j});
-                }
+    int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
+        vector<vector<int>> va, vb;
+        int rows = A.size(), cols = A[0].size();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (A[i][j]) va.push_back({i, j});
+                if (B[i][j]) vb.push_back({i, j});
             }
         }
-        int ans=0;
-        map<pair<int,int>,int>mp;
-        for(auto [i1,j1]:vec_a){
-            for(auto [i2,j2]:vec_b){
-                mp[{i1-i2,j1-j2}]++;
-                ans=max(ans,mp[{i1-i2,j1-j2}]);
+        unordered_map<string, int> mp;
+        for (auto &pa : va) {
+            for (auto &pb : vb) {
+                string s = to_string(pa[0] - pb[0]) + " " + to_string(pa[1] - pb[1]);
+                mp[s]++;
             }
         }
-        return ans;
+        int res = 0;
+        for (auto &it : mp) res = max(res, it.second);
+        return res;
     }
 };
