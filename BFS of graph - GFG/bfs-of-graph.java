816a685290717c648e0d6fe2c6b89b80ebd3c25f
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-11-14 09:18:21 +0530

    Added solution - LeetHub

---
+++ BFS of graph - GFG/bfs-of-graph.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        boolean[] visited = new boolean[V+1];
+        Queue<Integer> q = new ArrayDeque<>();
+        
+        q.add(0);
+        visited[0] = true;
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i<size; ++i){
+                int current = q.poll();
+                arr.add(current);
+                for(Integer x : adj.get(current)){
+                    if(!visited[x]){
+                        q.add(x);
+                        visited[x] = true;
+                    }
+                }
+            }
+        }
+        return arr;
+        
+    }
+}
